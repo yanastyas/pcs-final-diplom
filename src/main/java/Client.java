731b55fc79e4b73bd81d81cdfee199a2e09de5ab
yanastyas <yanastyas28@mail.ru>
2023-06-14 10:57:31 +1000

    commit2

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.Socket;
+
+public class Client {
+    static String request = "ЭТАП";
+
+    public static void main(String[] args) throws IOException {
+
+        var inetAddress = Inet4Address.getByName("localhost");
+
+        try (var socket = new Socket(inetAddress, 8989);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+            out.println(request);
+            String answer;
+            while ((answer = in.readLine()) != null) {
+                System.out.println("Результат с сервера: " + answer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
